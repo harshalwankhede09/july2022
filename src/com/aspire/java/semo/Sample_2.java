3e93106adee5a3a414b2fe4c762b76d47d12f894
lenovo <lenovo@LAPTOP-P8RKRJPS>
2022-07-26 05:14:02 -0700

    tjis is 2nd commit

--- src/com/aspire/java/semo/Sample_2.java
+++ src/com/aspire/java/semo/Sample_2.java
@@ -1,5 +1,12 @@
 package com.aspire.java.semo;
 
-public class Sample_2 {
-
+public class Sample_2
+{
+public static void main(String[] args) 
+{ int p=50;
+  int q=30;
+  int sub=p-q;
+  System.out.println(sub);
+	
+}
 }
